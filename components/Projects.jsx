67b69952c0cb78d97371e1397358692359943a0e
unknown <apurvaborhadee@gmail.com>
2023-03-01 14:39:36 +0530

    Projects Responsive

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,27 +9,30 @@ const myLoader = ({ src, width, quality }) => {
 const Projects = () => {
 
     useEffect(() => {
-        gsap.to(".projects", {
-            scrollTrigger: {
-                trigger: ".projects-wrapper",
-                start: "top top",
-                end:"+=4000 center",
-                scrub: true,
-                pin: true,
-            },
-            yPercent:-70,
-        })
+        if (window.screen.width > 1200) {
+
+            gsap.to(".projects", {
+                scrollTrigger: {
+                    trigger: ".projects-wrapper",
+                    start: "top top",
+                    end:"+=4000 center",
+                    scrub: true,
+                    pin: true,
+                },
+                yPercent:-70,
+            })
+        }
 
 
     }, [])
 
     return (
-        <div className='projects-wrapper grid grid-cols-2 lg:mt-36 relative' id='projects'>
+        <div className='projects-wrapper grid lg:grid-cols-2 grid-cols-1 lg:mt-36 relative' id='projects'>
             <div className=' items-center projects p-10 flex flex-col gap-2'>
             <video src="/landing.mp4" autoPlay muted id="landing-video"></video>
-            <video src="/landing.mp4" autoPlay muted id="landing-video"></video>
-            <video src="/landing.mp4" autoPlay muted id="landing-video"></video>
-            <video src="/landing.mp4" autoPlay muted id="landing-video"></video>
+            <video src="/Mas-Black-Shoe.mp4" autoPlay muted id="landing-video"></video>
+            <video src="/Banner-MAS.mp4.mp4" autoPlay muted id="landing-video"></video>
+            <video src="/MAS-BLCK-Card-Mint.mp4" autoPlay muted id="landing-video"></video>
 
                 {/* <iframe src="https://player.vimeo.com/video/798896249?h=5fb46e8c9a&loop=1&title=0&byline=0&portrait=0&sidedock=0&transparent=0" width="500" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
                 <iframe src="https://player.vimeo.com/video/798899250?h=5fb46e8c9a&loop=1&title=0&byline=0&portrait=0&sidedock=0&transparent=0" width="500" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
@@ -37,8 +40,8 @@ const Projects = () => {
                 <iframe src="https://player.vimeo.com/video/798901003?h=5fb46e8c9a&loop=1&title=0&byline=0&portrait=0&sidedock=0&transparent=0" width="500" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
                 <iframe src="https://player.vimeo.com/video/798898118?h=5fb46e8c9a&loop=1&title=0&byline=0&portrait=0&sidedock=0&transparent=0" width="500" height="500" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> */}
             </div>
-            <div className='projects-info mb-10 absolute bottom-0 right-0 p-10'>
-                <p className='text-6xl text-left'>WE TAKE GREAT PRIDE IN THE WORK WE DO</p>
+            <div className='projects-info mb-10 lg:absolute lg:bottom-0 lg:right-0 lg:p-10'>
+                <p className='lg:text-6xl text-4xl px-10 lg:px-0 lg:text-left text-center'>WE TAKE GREAT PRIDE IN THE WORK WE DO</p>
             </div>
         </div>
     )
